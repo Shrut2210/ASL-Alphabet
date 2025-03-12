e771834cf2ca04ce65d017beadb5dd3edc367dff
Shruti Vadgama <125206895+Shrut2210@users.noreply.github.com>
2025-03-13 00:10:47 +0530

    new code

--- app.py
+++ app.py
@@ -100,30 +100,36 @@ class VideoProcessor(VideoTransformerBase):
                 x_max = min(w, int(max([lm.x for lm in hand_landmarks.landmark]) * w + 30))
                 y_max = min(h, int(max([lm.y for lm in hand_landmarks.landmark]) * h + 30))
 
+                
                 cropped_hand = img[y_min:y_max, x_min:x_max]
 
-                if cropped_hand.size != 0 :
-                    resized_hand = cv2.resize(cropped_hand, (IMG_SIZE, IMG_SIZE))
-                    landmark_tensor = []
+                if cropped_hand.size != 0:
                     landmark_tensor = np.zeros((IMG_SIZE, IMG_SIZE))
-                    
-                    for idx in range(0,21) :
+
+                    for idx in range(21):  
                         x = int(hand_landmarks.landmark[idx].x * w)
                         y = int(hand_landmarks.landmark[idx].y * h)
-                        
-                        x_norm = min(int(((x - x_min) / (x_max - x_min)) * (IMG_SIZE - 1)), IMG_SIZE - 1)
-                        y_norm = min(int(((y - y_min) / (y_max - y_min)) * (IMG_SIZE - 1)), IMG_SIZE - 1)
-                        
+
+                        width = max(1, x_max - x_min)
+                        height = max(1, y_max - y_min)
+
+                        x_norm = min(int(((x - x_min) / width) * (IMG_SIZE - 1)), IMG_SIZE - 1)
+                        y_norm = min(int(((y - y_min) / height) * (IMG_SIZE - 1)), IMG_SIZE - 1)
+
                         landmark_tensor[y_norm, x_norm] = 1
-                    
-                    landmark_tensor = landmark_tensor.reshape(1, IMG_SIZE, IMG_SIZE, 1)
-                    
+
+                    landmark_tensor = landmark_tensor.astype("float32")  
+                    landmark_tensor = np.expand_dims(landmark_tensor, axis=(0, -1))  
+
+                    print("Landmark tensor shape:", landmark_tensor.shape) 
+
                     prediction = model.predict(landmark_tensor)
                     confidence = np.max(prediction)
                     predicted_class = classes[np.argmax(prediction)]
-                    
+
                     cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (0, 0, 255), 2)
                     cv2.putText(img, f"{predicted_class} ({confidence:.2f}%)", (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
         return img 
 
 webrtc_streamer(key="asl_live", video_processor_factory=VideoProcessor, rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
